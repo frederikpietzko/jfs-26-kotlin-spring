b3bb476b6c2c8ad4211bcd9ecf1686fb900fe32f
Frederik Pietzko <frederik.pietzko@jetbrains.com>
2026-07-06 21:08:32 +0200

    chore: rename package to petclinic, configure h2 and jpa

---
+++ src/main/java/com/example/petclinic/Jfs26KotlinSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jfs26KotlinSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jfs26KotlinSpringApplication.class, args);
+    }
+
+}
